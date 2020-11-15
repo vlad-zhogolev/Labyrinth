6bf3dee8df55a3b4044f2d5845ab9ea147c53272
zvladyuha <z.vladyuha@yandex.ru>
2020-11-16 02:54:00 +0300

    List available items

--- Assets/Scripts/Labyrinth/Items.cs
+++ Assets/Scripts/Labyrinth/Items.cs
@@ -7,10 +7,31 @@ namespace LabyrinthGame {
 namespace Labyrinth {
 public enum Item
 {
+    None,
     One,
     Two,
     Three,
     Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Eleven,
+    Twelve,
+    Thirteen,
+    Fourteen,
+    Fifteen,
+    Sixteen,
+    Seventeen,
+    Eighteen,
+    Nineteen,
+    Twenty,
+    TwentyOne,
+    TwentyTwo,
+    TwentyThree,
+    TwentyFour
 }
 
 } // namespace Labyrinth
