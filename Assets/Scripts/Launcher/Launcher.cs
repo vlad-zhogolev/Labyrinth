8b9681dae1e36d68880733f611cb25c709b0584d
zvladyuha <z.vladyuha@yandex.ru>
2020-12-06 23:59:12 +0300

    Add connect/disconnect to photon

--- Assets/Scripts/Launcher/Launcher.cs
+++ Assets/Scripts/Launcher/Launcher.cs
@@ -2,112 +2,115 @@
 using Photon.Pun;
 using Photon.Realtime;
 
-namespace launcher
-{
+namespace LabyrinthGame {
 
-    public class Launcher : MonoBehaviourPunCallbacks
+    namespace Launcher
     {
-        #region Private Serializable Fields
 
-
-        #endregion
-
-
-        #region Private Fields
-
-
-        /// <summary>
-        /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
-        /// </summary>
-        string gameVersion = "1";
-
-
-        #endregion
-
-
-        #region MonoBehaviour CallBacks
-
-
-        /// <summary>
-        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
-        /// </summary>
-        void Awake()
+        public class Launcher : MonoBehaviourPunCallbacks
         {
-            // #Critical
-            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
-            PhotonNetwork.AutomaticallySyncScene = true;
-        }
+            #region Private Serializable Fields
 
 
-        /// <summary>
-        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
-        /// </summary>
-        void Start()
-        {
-            Connect();
-        }
+            #endregion
 
 
-        #endregion
+            #region Private Fields
 
 
-        #region Public Methods
+            /// <summary>
+            /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+            /// </summary>
+            string gameVersion = "1";
 
 
-        /// <summary>
-        /// Start the connection process.
-        /// - If already connected, we attempt joining a random room
-        /// - if not yet connected, Connect this application instance to Photon Cloud Network
-        /// </summary>
-        public void Connect()
-        {
-            Debug.LogFormat("{0}: Connect() called.", GetType().Name);
-            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
-            if (PhotonNetwork.IsConnected)
+            #endregion
+
+
+            #region MonoBehaviour CallBacks
+
+
+            /// <summary>
+            /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+            /// </summary>
+            void Awake()
             {
-                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
-                PhotonNetwork.JoinRandomRoom();
+                // #Critical
+                // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+                PhotonNetwork.AutomaticallySyncScene = true;
             }
-            else
+
+            #endregion
+
+
+            #region Public Methods
+
+
+            /// <summary>
+            /// Start the connection process.
+            /// - If already connected, we attempt joining a random room
+            /// - if not yet connected, Connect this application instance to Photon Cloud Network
+            /// </summary>
+            public void Connect()
             {
-                // #Critical, we must first and foremost connect to Photon Online Server.
-                PhotonNetwork.ConnectUsingSettings();
-                PhotonNetwork.GameVersion = gameVersion;
+                Debug.LogFormat("{0}: Connect ot photon network.", GetType().Name);
+
+                if (!PhotonNetwork.IsConnectedAndReady)
+                {
+                    // #Critical, we must first and foremost connect to Photon Online Server.
+                    PhotonNetwork.ConnectUsingSettings();
+                    PhotonNetwork.GameVersion = gameVersion;
+                }
+
+                Debug.LogFormat("{0}: Is connected {1}", GetType().Name, PhotonNetwork.IsConnectedAndReady);
             }
+
+            public void Disconnect()
+            {
+                Debug.LogFormat("{0}: Disconnect ot photon network.", GetType().Name);
+
+                if (PhotonNetwork.IsConnectedAndReady)
+                {
+                    PhotonNetwork.Disconnect();
+                }
+
+                Debug.LogFormat("{0}: Is connected {1}", GetType().Name, PhotonNetwork.IsConnectedAndReady);
+            }
+
+
+            #endregion
+
+
+
+            #region MonoBehaviourPunCallbacks Callbacks
+
+            public override void OnConnectedToMaster()
+            {
+                Debug.LogFormat("{0}: OnConnectedToMaster() was called by PUN", GetType().Name);
+                //PhotonNetwork.JoinRandomRoom();
+            }
+
+            public override void OnDisconnected(DisconnectCause cause)
+            {
+                Debug.LogWarningFormat("{0}: OnDisconnected() was called by PUN with reason {1}", GetType().Name, cause);
+            }
+
+            public override void OnJoinRandomFailed(short returnCode, string message)
+            {
+                Debug.LogFormat("{0}:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom", GetType().Name);
+
+                // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+                PhotonNetwork.CreateRoom(null, new RoomOptions());
+            }
+
+            public override void OnJoinedRoom()
+            {
+                Debug.LogFormat("{0}: OnJoinedRoom() called by PUN. Now this client is in a room.", GetType().Name);
+            }
+
+            #endregion
         }
 
+    } // namespace Launcher
 
-        #endregion
-
-
-
-        #region MonoBehaviourPunCallbacks Callbacks
-
-        public override void OnConnectedToMaster()
-        {
-            Debug.LogFormat("{0}: OnConnectedToMaster() was called by PUN", GetType().Name);
-            PhotonNetwork.JoinRandomRoom();
-        }
-
-        public override void OnDisconnected(DisconnectCause cause)
-        {
-            Debug.LogWarningFormat("{0}: OnDisconnected() was called by PUN with reason {1}", GetType().Name, cause);
-        }
-
-        public override void OnJoinRandomFailed(short returnCode, string message)
-        {
-            Debug.LogFormat("{0}:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom", GetType().Name);
-
-            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
-            PhotonNetwork.CreateRoom(null, new RoomOptions());
-        }
-
-        public override void OnJoinedRoom()
-        {
-            Debug.LogFormat("{0}: OnJoinedRoom() called by PUN. Now this client is in a room.", GetType().Name);
-        }
-
-        #endregion
-    }
-
-} // namespace launcher
+} // namespace LabyrinthGame
