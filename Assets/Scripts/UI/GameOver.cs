3d4c7992c105bae44677a0c526af8080ae973677
zvladyuha <z.vladyuha@yandex.ru>
2020-12-17 15:46:20 +0300

    Exit from online game

--- Assets/Scripts/UI/GameOver.cs
+++ Assets/Scripts/UI/GameOver.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 
 namespace LabyrinthGame
 {
     namespace UI
     {
-        public class GameOver : MonoBehaviour
+        public class GameOver : MonoBehaviourPunCallbacks
         {
             public void GoToMainMenuHandler()
             {
+                Debug.LogFormat("{0}: Load start scene", GetType().Name);
                 SceneManager.LoadScene("StartScene");
+            }
+
+            public void GoToMainMenuOnlineHandler()
+            {
+                Debug.LogFormat("{0}: Leave room", GetType().Name);
+                Time.timeScale = 1;
+                PhotonNetwork.LeaveRoom();
+            }
+
+            public override void OnLeftRoom()
+            {
+                GoToMainMenuHandler();
             }
         }
     } // namespace UI
